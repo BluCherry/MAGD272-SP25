dc814af33a9c7133fea1b7cc47cd266581407c80
BluCherry <cgroves317@gmail.com>
2026-02-11 12:15:20 -0600

    bouncy scene

---
+++ Assets/follow.cs
@@ -0,0 +1,17 @@
+// Source - https://stackoverflow.com/a/65821523
+// Posted by Zaphod, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-11, License - CC BY-SA 4.0
+
+using UnityEngine;
+
+public class Follow_player : MonoBehaviour
+{
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 1, -5);
+    }
+}
